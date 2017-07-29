0be520e037fdcba046efd773764578bf51a41610
azpekt <azpekt@gmail.com>
2017-07-29 18:49:54 +0000

    added stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,14 @@ sudo yum update
 sudo yum install -y git
 sudo yum install -y screen
 
-git clone https://github.com/azpekt/configs.git 
+# getting vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/instal l_awesome_vimrc.sh
+
+# getting bash prompt
+cd ~
+git clone https://github.com/magicmonty/bash-git-pr ompt.git .bash-git-prompt --depth=1
+
+# getting configs
+git clone https://github.com/azpekt/configs.git
 
