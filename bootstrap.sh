07bb1d79b1fe3042d6a0b64f8d7a5b3208045313
nick <azpekt@gmail.com>
2018-03-18 21:42:12 +0000

    new configs with powerline and correct install for vim8 on centos

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+#adding vim repo to update it to vim8
+sudo curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo
 
-sudo yum update
+sudo yum update -y vim*
 
+# installing goodies
 sudo yum install -y git
 sudo yum install -y screen
-
-# getting vim
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/instal l_awesome_vimrc.sh
-
-# getting bash prompt
-cd ~
-git clone https://github.com/magicmonty/bash-git-pr ompt.git .bash-git-prompt --depth=1
 
 # getting configs
 git clone https://github.com/azpekt/configs.git
 
+# getting vim prompt repo
+rm -rf ~/.vim*
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# getting powerline shell config and installing
+git clone https://github.com/b-ryan/powerline-shell
+cd powerline-shell
+python setup.py install
+
+# copying bash config file to homedir
+cp config/.bashrc .
+
+# all done!
+echo "done, Nik - this baby is all yours...!"
