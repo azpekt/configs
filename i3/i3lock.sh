50b96893e38320b42b8c85e91bd953af6750d6fc
Ubuntu <ubuntu@ip-172-31-1-154.us-west-1.compute.internal>
2020-09-11 18:29:31 +0000

    removed old stuff

--- i3/i3lock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-convert $HOME/.config/i3/lock_icon.png -scale 100% /tmp/lock-icon.png
-convert /tmp/screen.png /tmp/lock-icon.png -gravity center -composite -matte /tmp/screen.png
-i3lock -f -i /tmp/screen.png
